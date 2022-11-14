63527e2e735bab5b1cb968953f2754f518a2abc1
Elchin M. Abbasov <elchinma@code.local>
2022-11-14 14:16:11 +0400

    Programm1

---
+++ Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+
+
+            //int age = 10;
+
+            //if (age >= 12)
+            //{
+            //    Console.WriteLine("10 boyuk deyl");
+
+            //}
+
+
+
+
+
+            //else if (age <= 12)
+            //{
+            //    Console.WriteLine("12 boyukdur");
+            //}
+            //string ad = "Elchin";
+            //switch (ad)
+            //{
+            //    case "Mustafa":
+            //        Console.WriteLine("Adi Mustafa");
+            //        break;
+
+            //    case "Elchin":
+            //        Console.WriteLine("Adi Elchin");
+            //        break;
+
+            //    case "Ilqar":
+            //        Console.WriteLine("Adi Ilgar");
+            //        break;
+            //    case "Mehman":
+            //        Console.WriteLine("Adi Mehman");
+            //        break;
+            //}
+
+
+
+
+
+            //for (int i = 10; i > 0; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //int number = 50;
+
+            //while (number < 0)
+            //{
+            //    Console.WriteLine(number);
+
+            //}
+
+
+
+
+            //var student = new
+            //{
+            //    firstname = "Cemil",
+            //    surname = "Isaev",
+            //    age = 23
+
+            //};
+            //Console.WriteLine(student.firstname);
+
+
+
+
+
+
+            //string[] student_name = { "Elcin", "Abbasov", "Xicran" };
+
+            //for (int i = 0; i < student_name.Length; i++)
+            //{
+            //    Console.WriteLine(student_name[i]);
+            //}
+
+            //int[,] numbers = { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 1, 2 } };
+
+            //Console.WriteLine(numbers[1, 1]);
+
+
+
+
+
+
+
+            //Console.WriteLine("Please choose number");
+            //string num = Console.ReadLine();
+
+            //Console.WriteLine("Choose number: " + num);
+
+
+
+
+
+
+
+            //Console.WriteLine("Please choose number");
+
+            //int num = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Result: ");
+
+            //if (num % 4 == 0 && num % 9 == 0) 
+            //{
+            //    Console.Write("Bolunur");
+            //}
+
+            //else
+            //{
+            //    Console.Write("Bolunmur");
+            //}
+
+
+
+        }
+
+    }
+}
+
+
+
+
+      
+        
